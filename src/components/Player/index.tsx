d8a319d2a1edf32dff7a4f2ec1dc8418cbeb96b8
Samuel Porto <samuelalcala2001@outlook.com>
2021-04-22 10:31:48 -0300

    Componente Player finalizado (Sem Podcast Rodando)

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,50 @@
+import styles from "./styles.module.scss";
+
+const Player = () => {
+  return (
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="Tocando Agora" />
+        <strong>Tocando Agora</strong>
+      </header>
+
+      <div className={styles.emptyPlayer}>
+        <strong>Selcione um Podcast para ouvir.</strong>
+      </div>
+
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            <div className={styles.emptySlyder} />
+          </div>
+          <span>00:00</span>
+        </div>
+
+        <div className={styles.buttons}>
+          <button type="button">
+            <img src="/shuffle.svg" alt="Emparalhar" />
+          </button>
+
+          <button type="button">
+            <img src="/play-previous.svg" alt="Tocar Anterior" />
+          </button>
+
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="Tocar" />
+          </button>
+
+          <button type="button">
+            <img src="/play-next.svg" alt="Tocar Proximo" />
+          </button>
+
+          <button type="button">
+            <img src="/repeat.svg" alt="Repetir" />
+          </button>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Player;
